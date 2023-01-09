8cca1b8c3ada9fa5768b2415a69114fde3f51e42
Anisree-S-S <96193534+Anisree-S-S@users.noreply.github.com>
2023-01-09 10:10:52 +0530

    spiner with intent

---
+++ activity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner1;
+    String clickitem;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner1=findViewById(R.id.spinner1);
+
+        String lang[]=new String[]{"java","perl"};
+        ArrayAdapter<String> arry=new ArrayAdapter<String>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,lang);
+        arry.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        spinner1.setAdapter(arry);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                clickitem=lang[i];
+                Toast.makeText(MainActivity.this, clickitem, Toast.LENGTH_SHORT).show();
+
+                Intent I=new Intent(MainActivity.this,page2.class);
+                I.putExtra("title",clickitem);
+                startActivity(I);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+}
